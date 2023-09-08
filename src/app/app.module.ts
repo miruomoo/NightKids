c7e5a262c865972da8288a90bc93d271ab910c61
Miruo <miruomoo@gmail.com>
2023-09-08 13:05:44 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { FailureComponent } from './failure/failure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent,
+    FailureComponent
   ],
   imports: [
     BrowserModule,
